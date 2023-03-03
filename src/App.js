b82a37f9ed68c41bc7b15867ce1b64bde557e9f3
farzaneh-haghani <farzaneh.haghani@gmail.com>
2023-03-03 02:16:32 +0000

    exercise E done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import Logo from "./Logo";
 import BestPokemon from "./BestPokemon";
 import CaughtPokemon from "./CaughtPokemon";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
+    <div className="App">
       <Logo appName="Pokedex" handleClick={logWhenClicked} />
       <BestPokemon abilities={["Anticipation", "Adaptability", "Run-Away"]} />
       <CaughtPokemon date={new Date().toLocaleDateString()} />
--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -3,16 +3,24 @@ import { useState } from "react";
 
 
 const CaughtPokemon = (props) => {
-  const [caught, setCaught] = useState(0);
+  // const [caught, setCaught] = useState(0);
+  const [caught, setCaught] = useState([]);
+  const pokemon = ["Squirtle", "Shuppet", "Ponyta"];
+
 
   const catchPokemon = () => {
-    setCaught(caught + 1);
+    // setCaught(caught + 1);
+    const randomElement = pokemon[Math.floor(Math.random() * (pokemon.length))];
+    setCaught(caught.concat(randomElement));
   };
 
   return (
     <>
-      <p>Caught {caught} Pokemon on {props.date}</p>
+      <p>Caught {caught.length} Pokemon on {props.date}</p>
       <button onClick={catchPokemon}>Click Me</button>
+      <ul>
+        {caught.map((eachCaught) => (<li>{eachCaught}</li>))}
+      </ul>
     </>
   );
 };
